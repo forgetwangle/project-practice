604cae5085aa686743246163348e93d2f4c9a171
Fierceplayers <61740086+forgetwangle@users.noreply.github.com>
2024-11-18 16:20:04 +0800

    Create 1.java

---
+++ 241118/001/1.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+public class ChargingSuccess {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String backendSuccessRate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "insert_time")
+    private Date insertTime;
+}
